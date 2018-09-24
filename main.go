a94377f8dd1f7a5e79ed62bc81d5d060fb05232c
Tom Stuart <thomascstuart@gmail.com>
2018-09-24 08:32:22 +0100

    Add Raft

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	pb "github.com/TomStuart92/asfalis/pkg/api"
 	"github.com/TomStuart92/asfalis/pkg/server"
+	"github.com/TomStuart92/asfalis/raft"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -16,6 +17,21 @@ const (
 )
 
 func main() {
+
+	storage := raft.NewMemoryStorage()
+
+	c := &raft.Config{
+		ID:              0x01,
+		ElectionTick:    10,
+		HeartbeatTick:   1,
+		Storage:         storage,
+		MaxSizePerMsg:   4096,
+		MaxInflightMsgs: 256,
+	}
+
+	peers := []raft.Peer{{ID: 0x01}}
+	raft.StartNode(c, peers)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
